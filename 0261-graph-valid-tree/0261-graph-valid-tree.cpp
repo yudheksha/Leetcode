251e5c4314601d239dfcea2502d514d13abe19d3
yudheksha <68909271+yudheksha@users.noreply.github.com>
2024-08-06 20:15:06 -0400

    Time: 20 ms (17.71%), Space: 17.9 MB (23.16%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector <int> ve[2001];
+    int vis[2001];
+    bool dfs(int s, int p){
+        vis[s] = 1;
+        bool res = true;
+        for(auto x: ve[s]){
+            if(x!=p){
+                if(vis[x])res = false;
+                else {
+                    res &= dfs(x,s);
+                }
+            }
+        }
+        return res;
+    }
+    bool validTree(int n, vector<vector<int>>& edges) {
+
+        for(auto e: edges){
+            ve[e[0]].push_back(e[1]);
+            ve[e[1]].push_back(e[0]);
+        }
+        bool ok = dfs(0,-1);
+        for(int i=0;i<n;i++){
+            if(!vis[i])ok=false;
+        }
+        return ok;
+    }
+};
